f7e3a21f22530785fbc8fbcfc2b92ab7e37c5dad
Deniz Köse <deniz8011@gmail.com>
2025-12-02 18:33:37 +0300

    feat: Introduce a dedicated rules page with a new base page component, update navigation links, and refactor source data.
    #152

--- src/config/books.ts
+++ src/config/books.ts
@@ -1,4 +1,19 @@
 // See ../lib/source.json
 // If empty, all books are active
 // export const ACTIVE_BOOKS_IDS = [2];
-export const ACTIVE_BOOKS_IDS = [];
+
+const sourceJson = await import("../lib/source.json");
+
+const legacyBooks = sourceJson.default.filter((book) => book.id < 205).map((book) => book.id);
+
+const remasterBooks = sourceJson.default.filter((book) => book.id >= 205).map((book) => book.id);
+
+export const ACTIVE_BOOKS_IDS = [
+    // ...legacyBooks, 
+    // ...remasterBooks
+    216, // Player Core
+    // 218, // GM Core
+    // 221, // Monster Core
+    // 227, // Player Core 2
+    // 236, // NPC Core
+];
